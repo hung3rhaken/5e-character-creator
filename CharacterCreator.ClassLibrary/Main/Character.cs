e3df027412fd34089cc0054b093f5b3b15e05057
Mike Löring <mike.loering@lorent-online.com>
2025-04-01 14:15:08 +0200

    Basic input & output and UI elements

---
+++ CharacterCreator.ClassLibrary/Main/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace CharacterCreator.ClassLibrary.Main;
+
+public class Character
+{
+    public string Name { get; set; } = string.Empty;
+    public string CharacterClass { get; set; } = string.Empty;
+    public string Species { get; set; } = string.Empty;
+    public int Level { get; set; }
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int Constitution { get; set; }
+    public int Intelligence { get; set; }
+    public int Wisdom { get; set; }
+    public int Charisma { get; set; }
+
+    public string FullTitle => $"{Name} the Great";
+}
